c631f8392fe065884f0ad6614a413a98b051f488
Clorith <bg3erl42>
2018-10-16 23:29:00 +0200

    Initial import to GitHub from Gists

---
+++ src/alsoviewing.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name          WordPress.org Also Viewing
+// @namespace     http://jason.stallin.gs
+// @description   See when another person is viewing the same post.
+// @author        Clorith
+// @grant         none
+// @include       https://*.wordpress.org/support/topic/*
+// @include       https://*.wordpress.org/support/view/*
+// @include       https://wordpress.org/support/topic/*
+// @include       https://wordpress.org/support/view/*
+// @require       https://code.jquery.com/jquery-1.11.0.min.js
+// @require       https://viewing-server.herokuapp.com/socket.io/socket.io.js
+// @version       0.0.8.0
+// ==/UserScript==
